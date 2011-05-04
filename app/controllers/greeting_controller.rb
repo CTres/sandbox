47a02b60c61aecad8cf6506b6c0101690e900c7e
Colin Treseler <treseler@gmail.com>
2011-05-03 23:25:26 -0400

    initial commit

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,9 @@
+class GreetingController < ApplicationController
+  def index
+    
+    @age=8
+    @table={"headings" => ["addend", "addend", "sum"]}
+          
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PilotProject::Application.config.secret_token = '3458647995f5c74a842053e7826644ab93a7a92296b9942d20df807aa42bb7a791bc7b7cba5391fea60e5a45c4797c6100a8ccb5903471ad66b8369689e48e16'
